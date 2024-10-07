8098848d2476b19935a66ac366a8c160cb8ea8c0
Duck <148314626+Rroq1@users.noreply.github.com>
2024-10-07 21:31:27 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// Sélectionner l'élément de la navbar
+const navbar = document.querySelector('#navbar');
+
+let navScroll = 0;
+
+window.addEventListener('scroll', () => {
+    // Vérifie si l'utilisateur a fait défiler vers le bas ou vers le haut
+    if (window.scrollY > navScroll) {
+        // Défilement vers le bas
+        navbar.style.top = '-100px'; // Ajuste cette valeur si nécessaire
+    } else {
+        // Défilement vers le haut
+        navbar.style.top = '0';
+    }
+
+    // Met à jour la valeur de navScroll
+    navScroll = window.scrollY;
+});
+
+// ScrollReveal configuration
+ScrollReveal({
+    reset: true,
+    distance: '50px',
+    duration: 2500,
+    delay: 100
+});
+
+ScrollReveal().reveal('.mb-title, .tgr-title, .tgr-content', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.mb-text, .ow-text', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.ow-title', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.tgr-text', { delay: 500, origin: 'bottom' });
